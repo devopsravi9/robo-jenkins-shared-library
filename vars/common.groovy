ddcea2de6213df8c4997efdab53df0f0e123935b
Ravi <ravi@gmail.com>
2022-09-23 16:57:03 +0530

    ec2 code

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,4 +1,9 @@
 def pipelineInit () {
+    sh '''
+        ls -ltr
+        rm -rf *
+        ls -ltr
+    '''
     git branch: 'main', url: 'https://github.com/devopsravi9/cart1.git'
     sh 'ls -ltr'
 }
