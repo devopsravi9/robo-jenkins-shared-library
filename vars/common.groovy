48df16e6c1438cadcd58ffe70e8068c485dee149
Ravi <ravi@gmail.com>
2022-09-23 18:54:05 +0530

    ec2 code

--- vars/common.groovy
+++ vars/common.groovy
@@ -8,11 +8,17 @@ def pipelineInit () {
 def publishArtifact () {
     stage ('prepare  artifact ') {
         if (env.APP_TYPE == 'nodejs') {
-            sh "zip -r ${COMPONENT}.zip server.js node_modules"
+            sh "zip -r ${COMPONENT}-${TAG_NAME}.zip server.js node_modules"
         }
         //if (env.APP_TYPE == 'maven') {
           //  sh "zip -r ${COMPONENT}'.zip"
         //}
+
+    stage ('upload artifact to nexus repo') {
+        sh """
+            curl --fail -u admin:admin123 --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.8.168:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}/
+        """
+    }
 
 
     }
