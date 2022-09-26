c9cf39a9ee1c1f6c4142322c98847364696de061
Ravi <ravi@gmail.com>
2022-09-26 18:41:12 +0530

    ec2 code

--- vars/common.groovy
+++ vars/common.groovy
@@ -56,37 +56,45 @@ def unittest () {
     }
 
 def publishArtifact () {
-    stage ('prepare  artifact ') {
+    env.ENV = dev
+    stage('prepare  artifact ') {
         if (env.APP_TYPE == 'nodejs') {
-            sh "zip -r ${COMPONENT}-${TAG_NAME}.zip server.js node_modules"
+            sh "zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip server.js node_modules"
 
         }
         if (env.APP_TYPE == 'maven') {
             sh " mv target/shipping-1.0.jar shipping.jar"
-            sh "zip -r ${COMPONENT}-${TAG_NAME}.zip  shipping.jar"
+            sh "zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip  shipping.jar"
         }
 
         if (env.APP_TYPE == 'python') {
             sh """
-                zip -r ${COMPONENT}-${TAG_NAME}.zip *.py payment.ini requirements.txt
+                zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip *.py payment.ini requirements.txt
             """
         }
 
         if (env.APP_TYPE == 'nginx') {
             sh """
                 cd static
-                zip -r ../${COMPONENT}-${TAG_NAME}.zip  *
+                zip -r ../${ENV}-${COMPONENT}-${TAG_NAME}.zip  *
             """
         }
+    }
 
 
-   stage ('upload artifact to nexus repo') {
+    stage ('upload artifact to nexus repo') {
         withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'pass', usernameVariable: 'user')]) {
             sh """
-                curl --fail -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.8.168:8081/repository/${COMPONENT}/
+                curl --fail -u ${user}:${pass} --upload-file ${ENV}-${COMPONENT}-${TAG_NAME}.zip http://172.31.8.168:8081/repository/${COMPONENT}/
             """
-            }
+
        }
    }
+
+    stage ('deploy to dev env') {
+
+    }
+
+
 }
 
