f96f5d38a7a25a4fbd8d055479002f5bf07a1657
Ravi <ravi@gmail.com>
2022-09-26 20:09:46 +0530

    ec2 code

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -8,6 +8,7 @@ def call () {
             sh 'mvn clean package'
         }
         common.testing ()
+
         if (env.BRANCH_NAME == env.TAG_NAME) {
             common.publishArtifact()
         }
--- vars/python.groovy
+++ vars/python.groovy
@@ -3,6 +3,7 @@ def call () {
     node {
 
         common.pipelineInit ()
+
         common.testing ()
 
         if (env.BRANCH_NAME == env.TAG_NAME) {
