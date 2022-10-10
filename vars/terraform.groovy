7f2f0c23b113edb711b2fcce38e3fbe8f14d9682
Ravi <ravi@gmail.com>
2022-10-10 12:17:03 +0530

    ec2 code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -7,6 +7,11 @@ def call () {
             ]),
         ])
 
+        stage ('code checkout') {
+            sh 'find . | sed -e "1d | xargs rm -rf *'
+            git branch: 'main', url: 'https://github.com/devopsravi9/robo-terraform-mutable.git'
+        }
+
         stage ('terraform init') {
             sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
         }
