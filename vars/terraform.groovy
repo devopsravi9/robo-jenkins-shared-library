0655bc232f8748dcaad92d37b38ab40405bd6919
Ravi <ravi@gmail.com>
2022-10-10 12:31:22 +0530

    ec2 code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -16,7 +16,7 @@ def call () {
             sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
         }
         stage ('terraform plan') {
-            sh 'terraform plan -backend-config=env/${ENV}-backend.tfvars'
+            sh 'terraform plan -var-file=env/${ENV}.tfvars'
         }
         stage ('terraform ${ACTION}') {
             sh 'terraform ${ACTION} -auto-approve -var-file=env/${ENV}.tfvars'
